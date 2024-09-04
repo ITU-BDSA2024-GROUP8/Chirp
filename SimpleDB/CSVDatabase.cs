26343b5e5e4cc9cd748374ab0d90056f862fae41
mhiv <mhiv@itu.dk>
2024-09-04 21:53:14 +0200

    Add CSVDatabase class and record Cheep
    Co-authored-by: Marcus <mhiv@itu.dk>

---
+++ SimpleDB/CSVDatabase.cs
@@ -0,0 +1,14 @@
+﻿namespace SimpleDB;
+
+public class CSVDatabase<T> : IDatabaseRepository<T>
+{
+    public IEnumerable<T> Read(int? limit = null)
+    {
+        throw new Exception();
+    }
+
+    public void Store(T record)
+    {
+        throw new Exception();
+    }
+}
--- SimpleDB/IDatabaseRepository.cs
+++ SimpleDB/IDatabaseRepository.cs
@@ -4,4 +4,5 @@ interface IDatabaseRepository<T>
 {
     public IEnumerable<T> Read(int? limit = null);
     public void Store(T record);
+    public record Cheep(string Author, string Message, long Timestamp);
 }
