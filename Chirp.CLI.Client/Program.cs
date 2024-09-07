b37b16ccc797985ecfd5109be33b3e3dba338513
Bror <broh@itu.dk>
2024-09-07 20:46:15 +0200

    Fixed a lot of things XD

--- Chirp.CLI.Client/Program.cs
+++ Chirp.CLI.Client/Program.cs
@@ -5,54 +5,9 @@ namespace Chirp.CLI.Client
 {
     public class App
     {
-        private const string usage = @"
-
-Usage:
-  chirp read <limit>
-  chirp cheep <message>
-  chirp (h | help)
-  chirp version
-
-Options:
-  h/help     Show this screen.
-  version     Show version.
-";
-
         static void Main(string[] args)
         {
-            var arguments = new Docopt().Apply(usage, args, version: "1.0", exit: true)!;
-            CSVDatabase<Cheep> db = new CSVDatabase<Cheep>();
-
-            if (arguments["read"].IsTrue)
-            {
-                if (arguments["<limit>"] == null)
-                {
-                    IEnumerable<Cheep> cheeps = db.Read();
-                    UserInterface.PrintCheeps(cheeps);
-                } else 
-                {
-                    int limit = int.Parse(arguments["<limit>"].ToString());
-                    IEnumerable<Cheep> cheeps = db.Read(limit);
-                    UserInterface.PrintCheeps(cheeps);
-                }
-               
-                
-            }
-            else if (arguments["cheep"].IsTrue)
-            {
-                string message = arguments["<message>"].ToString();
-                Cheep cheep = new Cheep(Environment.UserName, $"\"{message}\"", DateTimeOffset.Now.ToUnixTimeSeconds());
-                db.Store(cheep);
-                UserInterface.CheepStoredMSG();
-            }
-            else if (arguments["help"].IsTrue || arguments["h"].IsTrue)
-            {
-                Console.WriteLine(usage);
-            }
-            else if (arguments["version"].IsTrue)
-            {
-                Console.WriteLine("Chirp CLI version 1.0");
-            }
+            UserInterface.Run(args);
         }
 
         public record Cheep(string Author, string Message, long Timestamp);
--- Chirp.CLI.Client/UserInterface.cs
+++ Chirp.CLI.Client/UserInterface.cs
@@ -1,21 +1,74 @@
-﻿// Chirp.CLI.Client/UserInterface.cs
+﻿using SimpleDB;
+using DocoptNet;
+
 namespace Chirp.CLI.Client
 {
     public static class UserInterface
     {
-        public static void PrintCheeps(IEnumerable<App.Cheep> cheeps)
+        private const string usage = @"
+
+Usage:
+  chirp read <limit>
+  chirp readAll
+  chirp cheep <message>
+  chirp (h | help)
+  chirp version
+
+Options:
+  h/help     Show this screen.
+  version     Show version.
+";
+
+        public static void Run(string[] args)
+        {
+            var arguments = new Docopt().Apply(usage, args, version: "1.0", exit: true)!;
+            CSVDatabase<App.Cheep> db = new CSVDatabase<App.Cheep>();
+
+            HandleArguments(arguments, db);
+        }
+
+        private static void HandleArguments(IDictionary<string, ValueObject> arguments, CSVDatabase<App.Cheep> db)
+        {
+            if (arguments["read"].IsTrue)
+            {
+                var limit = int.Parse(arguments["<limit>"].ToString()); 
+                IEnumerable<App.Cheep> cheeps = db.Read(limit); 
+                PrintCheeps(cheeps);
+            }
+            else if (arguments["readAll"].IsTrue)
+            {
+                IEnumerable<App.Cheep> cheeps = db.Read();
+                PrintCheeps(cheeps);
+            }
+            else if (arguments["cheep"].IsTrue)
+            {
+                var message = arguments["<message>"].ToString();
+                App.Cheep cheep = new App.Cheep(Environment.UserName, $"\"{message}\"", DateTimeOffset.Now.ToUnixTimeSeconds());
+                db.Store(cheep);
+                CheepStoredMsg();
+            }
+            else if (arguments["help"].IsTrue || arguments["h"].IsTrue)
+            {
+                Console.WriteLine(usage);
+            }
+            else if (arguments["version"].IsTrue)
+            {
+                Console.WriteLine("Chirp CLI version 1.0");
+            }
+        }
+
+        private static void PrintCheeps(IEnumerable<App.Cheep> cheeps)
         {
             foreach (var cheep in cheeps)
             {
-                string timestamp = Util.FromSecondsToDateAndTime(cheep.Timestamp);
+                var timestamp = Util.FromSecondsToDateAndTime(cheep.Timestamp);
                 Console.WriteLine($"{cheep.Author} @ {timestamp}: {cheep.Message}");
             }
         }
 
-        public static void CheepStoredMSG()
+        private static void CheepStoredMsg()
         {
             Console.WriteLine("Cheep stored successfully!");
         }
     }
 }
-
