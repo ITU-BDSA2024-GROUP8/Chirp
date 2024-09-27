dd6741fd191ddeb6bc8dd977a8492e354abcb0ee
mhiv <mhiv@itu.dk>
2024-09-27 16:49:13 +0200

    Add relevant initial methods to DBFacade
    Co-authored-by: Marcus <mhiv@itu.dk>

--- Chirp.Razor/DBFacade.cs
+++ Chirp.Razor/DBFacade.cs
@@ -2,5 +2,23 @@
 
 public class DBFacade
 {
+    public void createDB()
+    {
+        
+    }
 
+    public string getDbPath()
+    {
+        return "";
+    }
+    
+    public bool dbExists(string path)
+    {
+        return false;
+    }
+
+    public void addDummyData()
+    {
+        
+    }
 }
