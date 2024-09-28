cba17a64c5ebc343eb284d3e731f1886d9bc2b28
mhiv <mhiv@itu.dk>
2024-09-28 15:23:24 +0200

    Add createDB functionality
    Co-authored-by: Marcus <mhiv@itu.dk>

--- Chirp.Razor/DBFacade.cs
+++ Chirp.Razor/DBFacade.cs
@@ -1,4 +1,8 @@
-﻿namespace Chirp.Razor;
+﻿using System.Reflection;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Data.Sqlite;
+
+namespace Chirp.Razor;
 
 public class DBFacade : IDBFacade
 {
@@ -11,7 +15,22 @@ public class DBFacade : IDBFacade
     
     public void createDB()
     {
-        Console.WriteLine("Created Database");
+        var embeddedProvider = new EmbeddedFileProvider(Assembly.GetExecutingAssembly());
+        using var reader = embeddedProvider.GetFileInfo("data/schema.sql").CreateReadStream();
+        using var sr = new StreamReader(reader);
+        
+        var query = sr.ReadToEnd();
+        
+        // Creates database file and executes SQL query from data/schema.sql file.
+        using (var connection = new SqliteConnection($"Data Source={_dbPath}"))
+        {
+            connection.Open();
+        
+            var command = connection.CreateCommand();
+            command.CommandText = query;
+        
+            command.ExecuteNonQuery();
+        }
     }
     
     public bool dbExists(string path)
