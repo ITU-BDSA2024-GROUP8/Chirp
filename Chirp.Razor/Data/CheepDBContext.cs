6388fa59ee1b8635463c3a9bcab2910260d7fb33
mhiv <mhiv@itu.dk>
2024-10-02 15:10:36 +0200

    Add CheepDBContext
    Co-authored-by: Marcus <mhiv@itu.dk>

---
+++ Chirp.Razor/Data/CheepDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Chirp.Razor.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Chirp.Razor.Data;
+
+public class CheepDBContext : DbContext
+{
+    public DbSet<Cheep> Cheeps { get; set; }
+    public DbSet<Author> Authors { get; set; }
+
+    public CheepDBContext(DbContextOptions<CheepDBContext> options) : base(options)
+    {
+        
+    }
+}
