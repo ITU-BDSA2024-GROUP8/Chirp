a04111e9691895d80c4c11c3ef3f25d3622c715f
Konradmeno <Konradmeno@gmail.com>
2024-10-23 12:31:46 +0200

    Add new Unit test Test_CreateNewCheep
    Co-authored-by: Konrad <koad@itu.dk>

--- tests/unitTest/UnitTest1.cs
+++ tests/unitTest/UnitTest1.cs
@@ -48,4 +48,38 @@ public class UnitTest1
         
         Assert.Equal(author, authorByName);
     }
+    
+    [Fact]
+    public async Task Test_CreateNewCheep()
+    {
+        //Initialize the database
+        using var context = await Util.CreateInMemoryDatabase();
+        
+        //Create the service
+        ICheepRepository cheepRepository = new CheepRepository(context);
+        
+        var author = await cheepRepository.NewAuthorAsync("testAuthor", "testAuthor@email.com");
+
+        var cheepsFromAuthor = await cheepRepository.GetCheepsFromAuthorAsync(1, author.Name);
+        
+        int cheepsCount = 0;
+        
+        foreach (var cheep in cheepsFromAuthor)
+        {
+            cheepsCount++;
+        }
+
+        Assert.Equal(0, cheepsCount);
+        
+        await cheepRepository.NewCheepAsync(author.Name, author.Email, "This is a new test cheep");
+        int newCheepsCount = 0;
+        var newCheepsFromAuthor = await cheepRepository.GetCheepsFromAuthorAsync(1, author.Name);
+        
+        foreach (var cheep in newCheepsFromAuthor)
+        {
+            newCheepsCount++;
+        }
+        
+        Assert.Equal(1, newCheepsCount);
+    }
 }
