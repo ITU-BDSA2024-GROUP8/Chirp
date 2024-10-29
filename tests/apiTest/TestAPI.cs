b4a21d6bae122a687ba62686193d300dc85d92ba
mhiv <mhiv@itu.dk>
2024-10-29 20:12:53 +0100

    Remove test logs

--- tests/apiTest/TestAPI.cs
+++ tests/apiTest/TestAPI.cs
@@ -6,21 +6,17 @@ public class TestAPI : IClassFixture<WebApplicationFactory<Program>>
 {
     private readonly WebApplicationFactory<Program> _fixture;
     private readonly HttpClient _client;
-    private readonly ITestOutputHelper _output;
 
-    public TestAPI(WebApplicationFactory<Program> fixture, ITestOutputHelper output)
+    public TestAPI(WebApplicationFactory<Program> fixture)
     {
         _fixture = fixture;
         _client = _fixture.CreateClient(new WebApplicationFactoryClientOptions { AllowAutoRedirect = true, HandleCookies = true });
-        _output = output;
     }
 
     [Fact]
     public async void CanSeePublicTimeline()
     {
-        _output.WriteLine("test");
         var response = await _client.GetAsync("/");
-        _output.WriteLine($"This is client {response}");
         response.EnsureSuccessStatusCode();
         var content = await response.Content.ReadAsStringAsync();
 
