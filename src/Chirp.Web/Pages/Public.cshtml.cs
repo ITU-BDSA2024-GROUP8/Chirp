85fb2c870986955d14b4fe11d324b65a9b7661e9
mhiv <mhiv@itu.dk>
2024-10-30 22:00:03 +0100

    Add OnPost to Public and UserTimeline.cshtml.cs

--- src/Chirp.Web/Pages/Public.cshtml.cs
+++ src/Chirp.Web/Pages/Public.cshtml.cs
@@ -1,4 +1,7 @@
 ﻿using Chirp.Core.DTOs;
+using Chirp.Infrastructure.Models;
+using Chirp.Web.Pages.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -6,12 +9,17 @@ namespace Chirp.Web.Pages;
 
 public class PublicModel : PageModel
 {
+    [BindProperty]
+    public CheepFormModel FormData { get; set; }
+    
     private readonly ICheepService _service;
+    private readonly UserManager<Author> _userManager;
     public required List<CheepDTO> Cheeps { get; set; }
 
-    public PublicModel(ICheepService service)
+    public PublicModel(ICheepService service, UserManager<Author> userManager)
     {
         _service = service;
+        _userManager = userManager;
     }
 
     public async Task<ActionResult> OnGet()
@@ -20,5 +28,25 @@ public class PublicModel : PageModel
         int page = int.TryParse(pageQuery, out page) ? Math.Abs(page) : 1;
         Cheeps = await _service.GetCheeps(page);
         return Page();
+    }
+
+    public async Task<ActionResult> OnPost()
+    {
+        if (!ModelState.IsValid)
+        {
+            return Page();
+        }
+
+        var author = await _userManager.GetUserAsync(User);
+        
+        await _service.PostCheep(new Cheep
+        {
+            Author = author,
+            AuthorId = author.Id,
+            Text = FormData.Message,
+            TimeStamp = DateTime.Now
+        });
+
+        return RedirectToPage();
     }
 }
--- src/Chirp.Web/Pages/UserTimeline.cshtml.cs
+++ src/Chirp.Web/Pages/UserTimeline.cshtml.cs
@@ -1,4 +1,7 @@
 ﻿using Chirp.Core.DTOs;
+using Chirp.Infrastructure.Models;
+using Chirp.Web.Pages.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -6,12 +9,17 @@ namespace Chirp.Web.Pages;
 
 public class UserTimelineModel : PageModel
 {
+    [BindProperty]
+    public CheepFormModel FormData { get; set; }
+    
     private readonly ICheepService _service;
+    private readonly UserManager<Author> _userManager;
     public required List<CheepDTO> Cheeps { get; set; }
 
-    public UserTimelineModel(ICheepService service)
+    public UserTimelineModel(ICheepService service, UserManager<Author> userManager)
     {
         _service = service;
+        _userManager = userManager;
     }
 
     public async Task<ActionResult> OnGet(string author)
@@ -20,5 +28,25 @@ public class UserTimelineModel : PageModel
         int page = int.TryParse(pageQuery, out page) ? Math.Abs(page) : 1;
         Cheeps = await _service.GetCheepsFromAuthor(page, author);
         return Page();
+    }
+    
+    public async Task<ActionResult> OnPost()
+    {
+        if (!ModelState.IsValid)
+        {
+            return Page();
+        }
+        
+        var author = await _userManager.GetUserAsync(User);
+        
+        await _service.PostCheep(new Cheep
+        {
+            Author = author,
+            AuthorId = author.Id,
+            Text = FormData.Message,
+            TimeStamp = DateTime.Now
+        });
+
+        return RedirectToPage();
     }
 }
