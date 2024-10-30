994d53b34d6ccf8cf5ba221ed19bbad5a9adefbb
mhiv <mhiv@itu.dk>
2024-10-30 22:36:40 +0100

    Add BaseCheepFormPage to avoid code duplication

--- src/Chirp.Web/Pages/Public.cshtml.cs
+++ src/Chirp.Web/Pages/Public.cshtml.cs
@@ -1,26 +1,15 @@
 ﻿using Chirp.Core.DTOs;
 using Chirp.Infrastructure.Models;
-using Chirp.Web.Pages.Models;
+using Chirp.Web.Pages.Base;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Chirp.Web.Pages;
 
-public class PublicModel : PageModel
+public class PublicModel : BaseCheepFormPage
 {
-    [BindProperty]
-    public CheepFormModel FormData { get; set; }
-    
-    private readonly ICheepService _service;
-    private readonly UserManager<Author> _userManager;
-    public required List<CheepDTO> Cheeps { get; set; }
-
-    public PublicModel(ICheepService service, UserManager<Author> userManager)
-    {
-        _service = service;
-        _userManager = userManager;
-    }
+    public PublicModel(ICheepService service, UserManager<Author> userManager) 
+        : base(service, userManager) {}
 
     public async Task<ActionResult> OnGet()
     {
@@ -28,25 +17,5 @@ public class PublicModel : PageModel
         int page = int.TryParse(pageQuery, out page) ? Math.Abs(page) : 1;
         Cheeps = await _service.GetCheeps(page);
         return Page();
-    }
-
-    public async Task<ActionResult> OnPost()
-    {
-        if (!ModelState.IsValid)
-        {
-            return Page();
-        }
-
-        var author = await _userManager.GetUserAsync(User);
-        
-        await _service.PostCheep(new Cheep
-        {
-            Author = author,
-            AuthorId = author.Id,
-            Text = FormData.Message,
-            TimeStamp = DateTime.Now
-        });
-
-        return RedirectToPage();
     }
 }
--- src/Chirp.Web/Pages/UserTimeline.cshtml.cs
+++ src/Chirp.Web/Pages/UserTimeline.cshtml.cs
@@ -1,26 +1,15 @@
 ﻿using Chirp.Core.DTOs;
 using Chirp.Infrastructure.Models;
-using Chirp.Web.Pages.Models;
+using Chirp.Web.Pages.Base;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Chirp.Web.Pages;
 
-public class UserTimelineModel : PageModel
+public class UserTimelineModel : BaseCheepFormPage
 {
-    [BindProperty]
-    public CheepFormModel FormData { get; set; }
-    
-    private readonly ICheepService _service;
-    private readonly UserManager<Author> _userManager;
-    public required List<CheepDTO> Cheeps { get; set; }
-
     public UserTimelineModel(ICheepService service, UserManager<Author> userManager)
-    {
-        _service = service;
-        _userManager = userManager;
-    }
+        : base(service, userManager) {}
 
     public async Task<ActionResult> OnGet(string author)
     {
@@ -28,25 +17,5 @@ public class UserTimelineModel : PageModel
         int page = int.TryParse(pageQuery, out page) ? Math.Abs(page) : 1;
         Cheeps = await _service.GetCheepsFromAuthor(page, author);
         return Page();
-    }
-    
-    public async Task<ActionResult> OnPost()
-    {
-        if (!ModelState.IsValid)
-        {
-            return Page();
-        }
-        
-        var author = await _userManager.GetUserAsync(User);
-        
-        await _service.PostCheep(new Cheep
-        {
-            Author = author,
-            AuthorId = author.Id,
-            Text = FormData.Message,
-            TimeStamp = DateTime.Now
-        });
-
-        return RedirectToPage();
     }
 }
