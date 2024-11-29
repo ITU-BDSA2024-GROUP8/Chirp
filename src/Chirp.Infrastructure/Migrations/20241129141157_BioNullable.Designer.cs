1be4f621a08b7e7883754ea6a06867ad97e90012
cstiitu <161447248+cstiitu@users.noreply.github.com>
2024-11-29 15:31:52 +0100

    Bio is nullable
    Co-Authored-By: Konradmeno <57835836+Konradmeno@users.noreply.github.com>

--- src/Chirp.Infrastructure/Migrations/20241129141157_BioNullable.Designer.cs
+++ src/Chirp.Infrastructure/Migrations/20241129141157_BioNullable.Designer.cs
@@ -3,16 +3,19 @@ using System;
 using Chirp.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
 
-namespace Chirp.Razor.Migrations
+namespace Chirp.Infrastructure.Migrations
 {
     [DbContext(typeof(ChirpDBContext))]
-    partial class ChirpDBContextModelSnapshot : ModelSnapshot
+    [Migration("20241129141157_BioNullable")]
+    partial class BioNullable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.10");
