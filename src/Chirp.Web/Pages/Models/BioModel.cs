32826ac0177220859cb5afa1a75f20d2f4711d32
Konradmeno <Konradmeno@gmail.com>
2024-12-05 13:20:42 +0100

    Add BioModel.cs

---
+++ src/Chirp.Web/Pages/Models/BioModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Chirp.Web.Pages.Models;
+
+public class BioModel
+{
+    [Required (ErrorMessage = "Bio can be over 200 characters")]
+    [StringLength(301)]
+    public required string? RouteName { get; set; }
+    public required bool IsMyBio { get; set; }
+}
