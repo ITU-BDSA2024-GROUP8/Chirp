0f2ed7d3a31cc6aa6ac69962a06e723c22c29103
Konradmeno <Konradmeno@gmail.com>
2024-12-10 16:18:16 +0100

    Refactor editBioButton.js script to a seperate file and inject script into _bioPartial.cshtml

---
+++ src/Chirp.Web/wwwroot/js/editBioButton.js
@@ -0,0 +1,19 @@
+﻿function enableEditing() {
+    const bioP = document.getElementById('authorBio');
+    const editButton = document.getElementById('editButton');
+    const editInput = document.getElementById('editInput');
+    const saveButton = document.getElementById('saveButton');
+    const form = document.getElementsByClassName('bio-form')[0];
+
+    // Hide the text and the edit button
+    bioP.style.display = 'none';
+    form.style.display = 'flex';
+    editButton.style.display = 'none';
+
+    // Focus on the input field
+    editInput.focus();
+
+    // Place the cursor at the end of the text
+    const length = editInput.value.length;
+    editInput.setSelectionRange(length, length);
+}
