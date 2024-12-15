eee14c6138af5b962973b50402ce954a710c3259
mhiv <mhiv@itu.dk>
2024-12-15 17:15:08 +0100

    Movie bio javascript into same file

--- src/Chirp.Web/wwwroot/js/bioScript.js
+++ src/Chirp.Web/wwwroot/js/bioScript.js
@@ -18,3 +18,17 @@
     const length = editInput.value.length;
     editInput.setSelectionRange(length, length);
 }
+
+function disableEditing(bio){
+    const bioP = document.getElementById('authorBio');
+    const editButton = document.getElementById('editButton');
+    const form = document.getElementsByClassName('bio-form')[0];
+    const cancelButton = document.getElementById('cancel-button');
+    const editInput = document.getElementById('editInput');
+
+    editInput.value = bio;
+    cancelButton.style.display = 'none';
+    form.style.display = 'none';
+    bioP.style.display = 'flex';
+    editButton.style.display = 'flex';
+}
